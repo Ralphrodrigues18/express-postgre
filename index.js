167894fafd2a0c43390c9a7c0478e7d782b22a51
Ralph Rodrigues <128393558+Ralphrodrigues18@users.noreply.github.com>
2025-05-03 18:51:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,19 +5,24 @@ import sequelize from './src/database/db.js';
 
 dotenv.config(); 
 
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Conexão com o banco de dados foi bem-sucedida.');
+
+    await sequelize.sync(); 
+    console.log('Modelos sincronizados.');
+
+  } catch (error) {
+    console.error('Erro ao conectar com o banco de dados: ', error);
+  }
+})();
+
 const app = express();
 
 app.use(express.json());
 app.use(router);
 
 
-sequelize.authenticate()
-  .then(() => {
-    console.log('Conexão com o banco de dados foi bem-sucedida.');
-    app.listen(8080, () => {
-      console.log('Servidor rodando na porta 8080.');
-    });
-  })
-  .catch((error) => {
-    console.error('Erro ao conectar com o banco de dados:', error);
-  });
+app.listen(8080)
